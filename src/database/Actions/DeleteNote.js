1e406fd660c3041fb415ed112436ec8618b92f53
Jedai Saboteur <dev@paradoxinversion.com>
2018-04-03 11:59:12 -0700

    Implement basic db get functions

--- src/database/Actions/DeleteNote.js
+++ src/database/Actions/DeleteNote.js
@@ -2,6 +2,7 @@ const Note = require("../Models/Note");
 
 const DeleteNote = async (noteId) => {
   try{
+    // XXX: This delete function should also handle removing from tagslist and groups
     await Note.findOneAndRemove({_id: noteId});
   } catch (e){
     throw e;
---
+++ src/database/Actions/GetGroups.js
@@ -0,0 +1,7 @@
+const Group = require("../Models/Group");
+
+const GetGroups = async () => {
+  return await Group.find({});
+}
+
+module.exports = GetGroups;
