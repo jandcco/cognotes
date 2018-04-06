bc2fbad83087d5adec81199da53705a3802eff79
Celeste De Santiago <celestedaphne@gmail.com>
2018-04-06 14:03:46 -0700

    Set up mongodburi for heroku

--- src/config/config.js
+++ src/config/config.js
@@ -7,11 +7,13 @@ module.exports = (()=>{
     return process.env.NODE_ENV;
   };
   const makeConfig = () => {
-    if (getEnv() === 'development'){
+  //  if (getEnv() === 'development'){
       require('dotenv').config({path:path.join(__dirname, "../../.env")});
-    }
+  //  }
+    console.log(`host: ${process.env.DB_HOST}, name: ${process.env.DB_NAME}, port: ${process.env.API_PORT}`);
     config = {
       db: {
+        mongoUri: process.env.MONGODB_URI,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME
       },
--- src/database/client.js
+++ src/database/client.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Config = require("../config/config").getConfig();
 const startClient = async function startClient()  {
   mongoose.Promise = global.Promise;
-
-  const connection = await mongoose.connect(`mongodb://${Config.db.host}/${Config.db.database}`);
+  let uri = (Config.db.mongoUri) ? Config.db.mongoUri : `mongodb://${Config.db.host}/${Config.db.database}`
+  const connection = await mongoose.connect(uri);
   console.log("Mongo has started");
 }
 
