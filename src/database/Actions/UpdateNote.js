408b39c3460e850a6f0f4ca87b21a4752dbb1d89
Jedai Saboteur <dev@paradoxinversion.com>
2018-04-06 13:46:29 -0700

    DB Fixes

--- src/database/Actions/UpdateNote.js
+++ src/database/Actions/UpdateNote.js
@@ -1,8 +1,9 @@
 const Note = require("../Models/Note");
 
-const EditNote = async (noteId, updatedText) => {
-  const note = await Note.findOne({"_id":noteId});
+const EditNote = async (noteId, updatedTitle, updatedText) => {
+  const note = await Note.findOne({_id:noteId});
   note.text = updatedText;
+  note.title = updatedTitle;
   await note.save();
   return note;
 };
